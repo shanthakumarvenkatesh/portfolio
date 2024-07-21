f7ed60fc1f901ed1ed3d0b6875162df05088f99f
Shanthakumar <shanthakumar.v@crediwatch.com>
2024-07-21 06:52:58 +0530

    updated timline

--- src/timeline.js
+++ src/timeline.js
@@ -21,8 +21,23 @@ function Timeline() {
                                                 <i className="icon-pen2" />
                                             </div>
                                             <div className="timeline-label">
-                                                <h2>Full Stack Web development course at PREBBYTES <span>2021-present</span></h2>
-                                                <p>I recently have joined the GUVI as a Full stack web development-student. My major part of the work has been into the field of creating sustainable and flexible Full Stack Websites using Reactjs as frontend and mongodb as backend and express as server.and some basic knowledge of AWS and also some Basics of DS and Algorithms.</p>
+                                                <h2>Associate Product Engineer at Crediwatch <span>2023-present</span></h2>
+                                                <p>Experience in integrating frontend applications with RESTful APIs, handling asynchronous data
+                                                fetching and updating UI components based on server responses. 
+                                                Experience in developing reusable and modular components in React.js. </p>
+
+                                            </div>
+                                        </div>
+                                    </article>
+                                    <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                                        <div className="timeline-entry-inner">
+                                            <div className="timeline-icon color-3">
+                                                <i className="icon-pen2" />
+                                            </div>
+                                            <div className="timeline-label">
+                                                <h2>UI Developer at Melissa Data Corporation <span>09/2021 - 08/2022</span></h2>
+                                                <p>Proficient in building responsive, accessible, and visually appealing websites using HTML, CSS, and
+                                                CSS frameworks like Bootstrap.</p>
                                             </div>
                                         </div>
                                     </article>
@@ -32,7 +47,7 @@ function Timeline() {
                                                 <i className="icon-pen2" />
                                             </div>
                                             <div className="timeline-label">
-                                                <h2>Postgraduation at Er Manimekalai college of Engineering<span>2018-2020</span></h2>
+                                                <h2>Postgraduation at Er Manimekalai college of Engineering <span>2018-2020</span></h2>
                                                 <p>I pursued my post-graduation studies with major in M.C.A.(Master of Computer Applications) with 7.6 cgpa. I have attended IOT workshop in college</p>
                                             </div>
                                         </div>
@@ -43,7 +58,7 @@ function Timeline() {
                                                 <i className="icon-pen2" />
                                             </div>
                                             <div className="timeline-label">
-                                                <h2>Undergraduation at M.G.R College<span>2015-2018</span></h2>
+                                                <h2>Undergraduation at M.G.R College<span>2015-2018 </span></h2>
                                                 <p>I have completed my Undergraduation with major in Computer Science with 66% cgpa. During my time at College, I have developed interest in solving complex problems in maths which helped me to improve my understanding of any problem.
 
                                                 </p>
